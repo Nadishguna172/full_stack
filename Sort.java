396c13954d0023af52145e4ae28b3b146ef3c72d
Nadishguna172 <145983929+Nadishguna172@users.noreply.github.com>
2024-11-20 15:14:11 +0530

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -2,7 +2,7 @@
 class Sort {
     public static void main(String[] args)
     {
-        int arr[] = { 4, 3, 2, 1 };
+        int arr[] = { 5,7,6,8};
         System.out.println("Bubble Sorting Program in java");
         System.out.println("Sorting output:");
         for (int i = 0; i < arr.length; i++) {
